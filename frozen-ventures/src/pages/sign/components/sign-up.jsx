3dc0c7ee1b00332403c145e5102db2100d0cb641
nirapyus <vjpcanaria@bpsu.edu.ph>
2024-06-14 14:19:10 +0800

    .

--- frozen-ventures/src/pages/sign/components/sign-up.jsx
+++ frozen-ventures/src/pages/sign/components/sign-up.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { ErrorMessage } from "../../../components/error-message";
 import { SuccessMessage } from "../../../components/success-message";
@@ -9,23 +9,48 @@ export const SignUp = () => {
   const [inputEmail, setInputEmail] = useState("");
   const [inputPhone, setInputPhone] = useState("");
   const [userRole, setUserRole] = useState("");
-  const [accountData, setAccountData] = useState({});
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [gender, setGender] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [address, setAddress] = useState({
+    street: "",
+    barangay: "",
+    municipality: "",
+    province: "",
+    zipCode: "",
+  });
+  const [currentStep, setCurrentStep] = useState(1);
   const [isSigningUp, setIsSigningUp] = useState(false);
   const [message, setMessage] = useState("");
   const [showErrorMessage, setShowErrorMessage] = useState(false);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
+  const handleNext = () => {
+    setCurrentStep((prevStep) => prevStep + 1);
+  };
+
+  const handleBack = () => {
+    setCurrentStep((prevStep) => prevStep - 1);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsSigningUp(true);
+
     const newAccountData = {
       email: inputEmail,
       phone: inputPhone,
       userRole: userRole,
       password: inputPass,
       confirmPass: inputCPass,
+      firstName: firstName,
+      lastName: lastName,
+      gender: gender,
+      birthdate: birthdate,
+      address: address,
     };
-    setAccountData(newAccountData);
+
     axios
       .post("http://localhost/prosen_bentures/api/signup.php", newAccountData)
       .then((response) => {
@@ -45,12 +70,23 @@ export const SignUp = () => {
         setMessage("Failed to create account");
         setShowErrorMessage(true);
       })
-
-      setTimeout(() => {
+      .finally(() => {
         setIsSigningUp(false);
         setShowErrorMessage(false);
         setShowSuccessMessage(false);
-      }, 3000);
+      });
+  };
+
+  const minBirthdate = new Date();
+  minBirthdate.setFullYear(minBirthdate.getFullYear() - 12);
+
+  const handleChangeBirthdate = (e) => {
+    const selectedDate = new Date(e.target.value);
+    if (selectedDate <= minBirthdate) {
+      setBirthdate(e.target.value);
+    } else {
+      console.error("User must be at least 12 years old.");
+    }
   };
 
   return (
@@ -58,77 +94,219 @@ export const SignUp = () => {
       {showErrorMessage && <ErrorMessage message={message} />}
       {showSuccessMessage && <SuccessMessage message={message} />}
       <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <div className="input-field">
-            <label htmlFor="userRole">User Role:</label>
-            <select
-              id="userRole"
-              name="userRole"
-              value={userRole}
-              onChange={(e) => setUserRole(e.target.value)}
-            >
-              <option value="" disabled>
-                Select a role
-              </option>
-              <option value="customer">Customer</option>
-              <option value="retailer">Retailer</option>
-              <option value="distributor">Distributor</option>
-              <option value="manufacturer">Manufacturer</option>
-            </select>
-          </div>
+        {currentStep === 1 && (
+          <div className="form-step">
+            <div className="input-container">
+              <div className="input-field">
+                <label htmlFor="userRole">User Role:</label>
+                <select
+                  id="userRole"
+                  name="userRole"
+                  value={userRole}
+                  onChange={(e) => setUserRole(e.target.value)}
+                >
+                  <option value="" disabled>
+                    Select a role
+                  </option>
+                  <option value="customer">Customer</option>
+                  <option value="retailer">Retailer</option>
+                  <option value="distributor">Distributor</option>
+                  <option value="manufacturer">Manufacturer</option>
+                </select>
+              </div>
+              <div className="input-field">
+                <label htmlFor="emailAdd">Email Address:</label>
+                <input
+                  type="email"
+                  id="emailAdd"
+                  name="emailAdd"
+                  value={inputEmail}
+                  onChange={(e) => setInputEmail(e.target.value)}
+                />
+              </div>
 
-          <div className="input-field">
-            <label htmlFor="emailAdd">Email Address:</label>
-            <input
-              type="text"
-              id="emailAdd"
-              name="emailAdd"
-              value={inputEmail}
-              onChange={(e) => setInputEmail(e.target.value)}
-            />
-          </div>
+              <div className="input-field">
+                <label htmlFor="phone">Phone:</label>
+                <input
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  value={inputPhone}
+                  onChange={(e) => setInputPhone(e.target.value)}
+                />
+              </div>
 
-          <div className="input-field">
-            <label htmlFor="phone">Phone:</label>
-            <input
-              type="number"
-              id="phone"
-              name="phone"
-              value={inputPhone}
-              onChange={(e) => setInputPhone(e.target.value)}
-            />
-          </div>
+              <div className="input-group">
+                <div className="input-field">
+                  <label htmlFor="password">Password:</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={inputPass}
+                    onChange={(e) => setInputPass(e.target.value)}
+                  />
+                </div>
 
-          <div className="password-container">
-            <div className="input-field">
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                value={inputPass}
-                onChange={(e) => setInputPass(e.target.value)}
-              />
+                <div className="input-field">
+                  <label htmlFor="confirmPassword">Confirm Password:</label>
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    value={inputCPass}
+                    onChange={(e) => setInputCPass(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="button-container first-next">
+                <button type="button" onClick={handleNext}>
+                  Next
+                </button>
+              </div>
             </div>
+          </div>
+        )}
+        {currentStep === 2 && (
+          <div className="form-step">
+            <div className="input-container">
+              <div className="input-group">
+                <div className="input-field">
+                  <label htmlFor="firstName">First Name:</label>
+                  <input
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </div>
+                <div className="input-field">
+                  <label htmlFor="lastName">Last Name:</label>
+                  <input
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </div>
+              </div>
 
-            <div className="input-field">
-              <label htmlFor="confirmPassword">Confirm Password:</label>
-              <input
-                type="password"
-                id="confirmPassword"
-                name="confirmPassword"
-                value={inputCPass}
-                onChange={(e) => setInputCPass(e.target.value)}
-              />
+              <div className="input-field">
+                <label htmlFor="gender">Gender:</label>
+                <input
+                  type="text"
+                  id="gender"
+                  name="gender"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                />
+              </div>
+              <div className="input-field">
+                <label htmlFor="birthdate">Birthdate:</label>
+                <input
+                  type="date"
+                  id="birthdate"
+                  name="birthdate"
+                  value={birthdate}
+                  onChange={handleChangeBirthdate}
+                  max={minBirthdate.toISOString().split("T")[0]}
+                />
+              </div>
+              <div className="button-container">
+                <button type="button" onClick={handleBack}>
+                  Back
+                </button>
+                <button type="button" onClick={handleNext}>
+                  Next
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        )}
+        {currentStep === 3 && (
+          <div className="form-step">
+            <div className="input-container">
+              <div className="input-field">
+                <label htmlFor="street">Street:</label>
+                <input
+                  type="text"
+                  id="street"
+                  name="street"
+                  value={address.street}
+                  onChange={(e) =>
+                    setAddress({ ...address, street: e.target.value })
+                  }
+                />
+              </div>
 
-        <div className="button-container">
-          <button type="submit" disabled={isSigningUp}>
-            {isSigningUp ? "Signing Up..." : "Sign Up"}
-          </button>
-        </div>
+              <div className="input-group">
+                <div className="input-field">
+                  <label htmlFor="barangay">Barangay:</label>
+                  <input
+                    type="text"
+                    id="barangay"
+                    name="barangay"
+                    value={address.barangay}
+                    onChange={(e) =>
+                      setAddress({ ...address, barangay: e.target.value })
+                    }
+                  />
+                </div>
+                <div className="input-field">
+                  <label htmlFor="municipality">Municipality:</label>
+                  <input
+                    type="text"
+                    id="municipality"
+                    name="municipality"
+                    value={address.municipality}
+                    onChange={(e) =>
+                      setAddress({ ...address, municipality: e.target.value })
+                    }
+                  />
+                </div>
+              </div>
+
+              <div className="input-group">
+                <div className="input-field">
+                  <label htmlFor="province">Province:</label>
+                  <input
+                    type="text"
+                    id="province"
+                    name="province"
+                    value={address.province}
+                    onChange={(e) =>
+                      setAddress({ ...address, province: e.target.value })
+                    }
+                  />
+                </div>
+                <div className="input-field">
+                  <label htmlFor="zipCode">Zip Code:</label>
+                  <input
+                    type="text"
+                    id="zipCode"
+                    name="zipCode"
+                    value={address.zipCode}
+                    onChange={(e) =>
+                      setAddress({ ...address, zipCode: e.target.value })
+                    }
+                  />
+                </div>
+              </div>
+
+              <div className="button-container">
+                <button type="button" onClick={handleBack}>
+                  Back
+                </button>
+                <button type="submit" disabled={isSigningUp}>
+                  {isSigningUp ? "Signing Up..." : "Sign Up"}
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </form>
     </div>
   );
